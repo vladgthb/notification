f11d1af49e9ac9f0799938f74135fe6222d39395
vladimirdanielyan <vladimirdanielyan@gmail.com>
2025-02-27 03:02:24 +0700

    added client socket.ts

---
+++ client/src/socket.ts
@@ -0,0 +1,26 @@
+import { io, Socket } from 'socket.io-client';
+
+let socket: Socket | null = null;
+
+/**
+ * Initialize Socket.io for a given userId.
+ */
+export function initSocket(userId: string) {
+    socket = io('http://localhost:4000', {
+        query: { userId },
+        transports: ['websocket']
+    });
+    socket.on('connect', () => {
+        console.log('Socket connected for user:', userId);
+    });
+    socket.on('connect_error', (err) => {
+        console.error('Socket connect error:', err);
+    });
+}
+
+/**
+ * Getter for the socket instance.
+ */
+export function getSocket(): Socket | null {
+    return socket;
+}
